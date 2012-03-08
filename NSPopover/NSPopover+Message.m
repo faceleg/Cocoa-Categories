29bf55376d22eebcfe7dfe00010035adbaba7230
Michael Robinson <mike@pagesofinterest.net>
2012-03-08 22:41:39 +1300

    Added NSPopover+Message category, updated Readme.

---
+++ NSPopover/NSPopover+Message.m
@@ -0,0 +1,63 @@
+//
+//  NSPopover+Instance.m
+//  Requires NSString+Size, available at: https://github.com/faceleg/Cocoa-Categories
+//
+//  Created by Michael Robinson <mike@pagesofinterest.net> on 8/03/12.
+//  Copyright (c) 2012 Code of Interest. All rights reserved.
+//
+
+#import "NSPopover+Message.h"
+#import "NSString+Size.h"
+
+@implementation NSPopover (Message)
+
++ (void) showRelativeToRect:(NSRect)rect
+                     ofView:(NSView *)view
+              preferredEdge:(NSRectEdge)edge
+                 string:(NSString *)string   
+                   maxWidth:(float)width {
+
+    
+    float padding = 15;
+
+    NSSize size = [string sizeWithWidth:width
+                                andFont:[NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:NSRegularControlSize]]];
+    
+    NSSize popoverSize = NSMakeSize(size.width + (padding * 2), size.height + (padding * 2));
+    NSRect popoverRect = NSMakeRect(0, 0, popoverSize.width, popoverSize.height);
+    
+    NSTextField *label = [[[NSTextField alloc] initWithFrame:NSMakeRect(padding, padding, size.width, size.height)] retain];
+                          
+    [label setBezeled:NO];
+    [label setDrawsBackground:NO];
+    [label setEditable:NO];
+    [label setSelectable:NO];
+    [label setStringValue:string];
+    [[label cell] setLineBreakMode:NSLineBreakByWordWrapping];
+    
+    NSView *container = [[[NSView alloc] initWithFrame:popoverRect] retain];
+    [container addSubview:label];
+    [label setBounds:NSMakeRect(padding, padding, size.width, size.height)];
+    [container awakeFromNib];
+
+    NSViewController *controller = [[[NSViewController alloc] init] retain];
+    [controller setView:container];
+
+    NSPopover *popover = [[[NSPopover alloc] init] retain];
+
+    [popover setContentSize:popoverSize];
+    [popover setContentViewController:controller];
+    [popover setAnimates:YES];
+    [popover setBehavior:NSPopoverBehaviorTransient];
+
+    [popover showRelativeToRect:rect
+                         ofView:view
+                  preferredEdge:edge];
+    [popover release];
+    [controller release];
+    [container release];
+    [label release];
+    return;
+}
+
+@end
--- README.md
+++ README.md
@@ -16,3 +16,26 @@ Adds the following:
 The class method `+ (NSColor *) colorWithHex:(NSString *)hexColor` attempts to initialise a new NSColor object from the given hexadecimal string. It will accept a three or six character string (# is optional). Anything else and it will raise an exception.
 
 The instance method `- (NSString *) hexColor;` returns a hexadecimal string representation of the given NSColor. If the colour's `colorSpaceName` is not one of: `NSCalibratedWhiteColorSpace`, `NSCalibratedBlackColorSpace`, `NSCalibratedRGBColorSpace` or `NSDeviceRGBColorSpace`, the method will return `@"transparent"`.
+
+## NSPopover+Message
+Adds the class method:
+
+```ObjectiveC
++ (void) showRelativeToRect:(NSRect)rect
+                     ofView:(NSView *)view
+              preferredEdge:(NSRectEdge)edge
+                     string:(NSString *)string   
+                   maxWidth:(float)width;
+```
+
+Intended to allow one to show a NSPopover containing a message with the minimum of fuss.
+
+Example:
+
+```ObjectiveC
+[NSPopover showRelativeToRect:[view bounds]
+                       ofView:view
+                preferredEdge:NSMaxXEdge
+                       string:@"Your message - the NSPopover will be as tall as required depending on your given maxWidth"
+                     maxWidth:200.0];
+```
